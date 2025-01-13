c1c9e1f6a0ce5fab4052f75c914581b307fd9465
Rodrigo Marques <rodrigolorensimarques@protonmail.com>
2025-01-13 10:29:00 -0300

    refact: Altera indicação de pasta raiz no Dockerfile

---
+++ WebApi/Migrations/20250113132643_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "VARCHAR(20)", maxLength: 20, nullable: false),
+                    first_name = table.Column<string>(type: "VARCHAR(20)", maxLength: 20, nullable: false),
+                    last_name = table.Column<string>(type: "VARCHAR(30)", maxLength: 30, nullable: false),
+                    email = table.Column<string>(type: "VARCHAR(50)", maxLength: 50, nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    role = table.Column<string>(type: "VARCHAR(20)", maxLength: 20, nullable: false),
+                    create_date = table.Column<DateTime>(type: "DATETIME2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
--- WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4130c43ef8fbe4eb5ad54617d85934c284bc003a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd5cdc480ee236c4f155dd023b1bfe086bf763c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
